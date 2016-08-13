fb8779871e9b2149fb6a8792d7b2f6f0792c20e8
johnmryan <johnmryan830@gmail.com>
2016-08-13 00:43:55 -0400

    Testing config vars, added skeleton post API

---
+++ src/server/item-controller.js
@@ -0,0 +1,10 @@
+//var firebase = require("firebase");
+//firebase.initializeApp({
+//      serviceAccount: "path/to/serviceAccountCredentials.json",
+//      databaseURL: process.env.FIREBASE_URL
+//});
+
+module.exports.useItem = function(req, res) {
+    console.log(process.env.FIREBASE_URL);
+    res.send({"success":process.env.FIREBASE_URL});
+}
