e9ed0e8a79ec373eef46c8234be9533a9e64155c
johnmryan <johnmryan830@gmail.com>
2016-08-13 12:11:57 -0400

    Updates

--- src/server/attack-controller.js
+++ src/server/attack-controller.js
@@ -1,5 +1,20 @@
 var firebase = require("./db.js");
+var db = firebase.database();
+var ref = db.ref("gamestate1");
 
 module.exports.attack = function(req, res) {
-    console.log("attack called");
+    //add event for given attack
+    var pokeAttack = req.body.attack;
+    var pokeName = req.body.pokemon;
+    var eventRef = ref.child("events");
+    eventRef.push(pokeName + " used " + pokeAttack + "!");
+    
+    //decrease health of opposing pokemon by corresponding amount
+    ref.on("value", function(snapshot) {
+        var gs = snapshot.val();
+        var currentPlayer = gs.currentPlayer;
+        var attackSnap = snapshot.child("player" + currentPlayer).child("pokemon").child(pokeName).child("attacks").child(pokeAttack).val();
+        var damage = attackSnap.power;
+        res.json({"success":"true"});
+    });
 }
--- src/server/pokemon-controller.js
+++ src/server/pokemon-controller.js
@@ -3,11 +3,10 @@ var db = firebase.database();
 var ref = db.ref("gamestate1");
 
 module.exports.choosePokemon = function(req, res) {
-    console.log(req.body);
     var player = req.body.player;
     var pokemon = req.body.pokemon;
     var playerRef = ref.child("player" + player);
-    playerRef.set({
+    playerRef.update({
         currentPokemon: pokemon
     });
     res.send({"success":"true"});
